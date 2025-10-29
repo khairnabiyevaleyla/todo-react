13b9d0337c899b2239193380121d5608319d8599
Leyla Khairnabiyeva <leylakhairnabiyeva@Leylas-MacBook-Air.local>
2025-10-29 20:53:49 +0400

    Add Profile dropdown with i18n

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,7 +1,7 @@
 import React from "react";
-import LeftSide from "./LeftSide";
-import SearchBar from "./SearchBar";
-import RightSide from "./RightSide";
+import LeftSide from "./LeftSide/LeftSide.tsx";
+import SearchBar from "./SearchBar/SearchBar.tsx";
+import RightSide from "./RightSide/RightSide.tsx";
 import style from "./navbar.module.scss"
 
 const Navbar = () => {
