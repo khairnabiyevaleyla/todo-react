dbe9e04245ded4c3aae433d395e8ad77ae944b3f
Leyla Khairnabiyeva <leylakhairnabiyeva@Leylas-MacBook-Air.local>
2025-11-16 13:29:52 +0400

    Refactor: update dropdown styling and animation

--- src/components/navbar/NavRightSide/NavRightSide.tsx
+++ src/components/navbar/NavRightSide/NavRightSide.tsx
@@ -1,51 +1,64 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import style from "./NavRightSide.module.scss";
 import { useTranslation } from "react-i18next";
-import i18next from "i18next";
 
 const NavRightSide = () => {
-  const { i18n } = useTranslation();
-  const [dropdownOpen, setDropdownOpen] = React.useState(false);
-  const { t } = useTranslation();
+  const { i18n, t } = useTranslation();
+  const [dropdownOpen, setDropdownOpen] = useState(false);
+  const dropdownRef = useRef<HTMLDivElement>(null);
 
   const handleLanguageChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
     i18n.changeLanguage(e.target.value);
   };
 
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (
+        dropdownRef.current &&
+        !dropdownRef.current.contains(event.target as Node)
+      ) {
+        setDropdownOpen(false);
+      }
+    };
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
+
   return (
-    <>
-      <div className={style.NavRightSide}>
-        <div>
-          {" "}
-          <select
-            className={style.languageSelect}
-            onChange={handleLanguageChange}
-            value={i18n.language}
-          >
-            <option value="en">EN</option>
-            <option value="ru">RU</option>
-            <option value="az">AZ</option>
-          </select>
-        </div>
+    <div className={style.NavRightSide}>
+      <select
+        className={style.languageSelect}
+        onChange={handleLanguageChange}
+        value={i18n.language}
+      >
+        <option value="en">EN</option>
+        <option value="ru">RU</option>
+        <option value="az">AZ</option>
+      </select>
+
+      <div
+        className={style.profileIcon}
+        onClick={() => setDropdownOpen((prev) => !prev)}
+        ref={dropdownRef}
+      >
+        <i className="ri-account-circle-2-line"></i>
         <div
-          className={style.profileIcon}
-          onClick={() => setDropdownOpen((prev) => !prev)}
+          className={`${style.profileDropdown} ${
+            dropdownOpen ? style.open : ""
+          }`}
         >
-          {" "}
-          <i className="ri-account-circle-2-line"></i>
-          {dropdownOpen && (
-            <div className={style.profileDropdown}>
-              <ul>
-                <li>{t("navbar.profile-dropdown.profile")}</li>
-                <li>
-                  <i className="ri-logout-circle-line"></i> {t("navbar.profile-dropdown.logout")}
-                </li>
-              </ul>
-            </div>
-          )}
+          <ul>
+            <li>{t("navbar.profile-dropdown.profile")}</li>
+            <li>
+              <i className="ri-logout-circle-line"></i>{" "}
+              {t("navbar.profile-dropdown.logout")}
+            </li>
+          </ul>
         </div>
       </div>
-    </>
+    </div>
   );
 };
 
